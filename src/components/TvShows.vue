e3c3627b63a2ed08416251b04b695e7fb66d5742
sulthan <muhammadsultana@outlook.com>
2020-08-31 21:40:16 +0700

    add filtering

--- src/components/TvShows.vue
+++ src/components/TvShows.vue
@@ -1,6 +1,12 @@
 /* eslint-disable max-len */
 <template>
   <div class="hello">
+    <h1>Filtered Shows</h1>
+    <div v-for="genre in genres" :key="genre.index">
+      {{genre}}
+      <!-- {{featuredMovies}} -->
+    </div>
+    <hr>
     <h1>Shows</h1>
     <div v-for="show in movies" :key="show.id">
       <b-card
@@ -32,14 +38,50 @@ export default {
   name: 'TvShows',
   data: () => ({
     movies: [],
+    genres: [],
+    featuredMovies: [],
   }),
   props: {
     msg: String,
   },
   mounted() {
     this.fetchShows();
+    this.fetchGenres();
   },
   methods: {
+    filteredShows() {
+      this.genres.forEach((value) => {
+        axios.get(`http://api.tvmaze.com/shows?genres=${value}`)
+          .then((resp) => {
+            const data = resp.data.splice(0, 4);
+            this.featuredMovies.push(data);
+            // debugger;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    },
+    fetchGenres() {
+      axios.get('http://api.tvmaze.com/shows')
+        .then((response) => {
+          const genreResp = [];
+          response.data.forEach((value) => {
+            value.genres.forEach((val) => {
+              genreResp.push(val);
+            });
+          });
+          this.genres = [...new Set(genreResp)];
+          this.genres = this.genres.splice(0, 4);
+          console.log(this.genres);
+          this.$nextTick(() => {
+            this.filteredShows();
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     fetchShows() {
       axios.get('http://api.tvmaze.com/shows')
         .then((response) => {
